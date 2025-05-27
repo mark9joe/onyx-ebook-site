62de6604213f3f90cc3fa462d918e0e2cf624945
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-05-27 10:48:00 +0530

    Update generate_pages.py

--- generate_pages.py
+++ generate_pages.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime, timezone
 
 # Paths
-LOCATIONS_FILE = "onyx-ebook-site/locations.txt"
+LOCATIONS_FILE = "locations.txt"
 OUTPUT_DIR = "generated_pages"
 
 # Create output directory if it doesn't exist
