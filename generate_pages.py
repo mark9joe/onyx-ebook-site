c739f783a9ad4a96437800cb626656128d6ac117
mark9joe <136325567+mark9joe@users.noreply.github.com>
2025-05-29 00:29:05 +0530

    Update generate_pages.py

--- generate_pages.py
+++ generate_pages.py
@@ -68,7 +68,7 @@ for location in locations:
 with open("sitemap.xml", "w", encoding="utf-8") as f:
     f.write("""<?xml version="1.0" encoding="UTF-8"?>\n""")
     f.write("""<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n""")
-    for url in sitemap_urls:
+    for url in sitemap_entries:
         f.write(f"""  <url><loc>{url}</loc></url>\n""")
     f.write("""</urlset>""")
 
